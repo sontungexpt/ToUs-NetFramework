2bbd15b94e1d8cbacd0fcf2e111d537582c49f95
HiImMalip <winqti@gmail.com>
2022-12-26 15:21:54 +0700

    Thu lai coi xem co conflict k

--- ToUs/ViewModel/PreviewViewModel/PreviewViewModel.cs
+++ ToUs/ViewModel/PreviewViewModel/PreviewViewModel.cs
@@ -10,33 +10,11 @@ namespace ToUs.ViewModel.PreviewViewModel
 {
     public class PreviewViewModel : ViewModelBase
     {
-        public ObservableCollection<Class> _class;
 
         public PreviewViewModel()
         {
-            _class = new ObservableCollection<Class>();
-            _class.Add(new Class(2, "12"));
-            _class.Add(new Class(3, "23"));
-            _class.Add(new Class(5, "34"));
-            _class.Add(new Class(6, "678"));
-            _class.Add(new Class(2, "34"));
-            _class.Add(new Class(4, "45"));
-            _class.Add(new Class(4, "678"));
-        }
-           
+
+        }         
     }
 
-    public class Class
-    {
-        public int day { get; set; }
-        public string lesson { get; set; }
-
-        public Class(int day, string lesson)
-        {
-            this.day = day;
-            this.lesson = lesson;
-        }
-    }
-
-   
 }
