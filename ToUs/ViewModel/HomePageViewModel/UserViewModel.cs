6583e08d46b8d99f41999b3816c68e5ad1e6e7a2
ngphth1312 <ngphth1312@gmail.com>
2023-02-02 20:54:04 +0700

    update add command usermodeview

--- ToUs/ViewModel/HomePageViewModel/UserViewModel.cs
+++ ToUs/ViewModel/HomePageViewModel/UserViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using ToUs.Models;
 using ToUs.Utilities;
 
@@ -13,6 +15,8 @@ namespace ToUs.ViewModel.HomePageViewModel
         //Fields:
         private int _selectedSchoolYear; //2 cái này để binding lựa chọn.
         private string _selectedSemester;
+        private string _tableName;
+        private bool[] _isCheckedArray;
 
         //Properties:
         public List<int> SchoolYears = new List<int>() //2 cái List cho Combobox
@@ -29,6 +33,26 @@ namespace ToUs.ViewModel.HomePageViewModel
             "HK2",
             "HK hè"
         };
+
+        public string TableName
+        {
+            get { return _tableName; }
+            set
+            {
+                _tableName = value;
+                OnPropertyChanged(nameof(TableName));   
+            }
+        }
+
+        public bool[] IsCheckedArray
+        {
+            get { return _isCheckedArray; }
+            set
+            {
+                _isCheckedArray = value;
+                OnPropertyChanged(nameof(IsCheckedArray));
+            }
+        }
 
         public int SelectedSchoolYear
         {
@@ -47,6 +71,23 @@ namespace ToUs.ViewModel.HomePageViewModel
             {
                 _selectedSemester = value;
                 OnPropertyChanged(nameof(SelectedSemester));
+            }
+        }
+
+        //Commands:
+        public ICommand SaveTableCommand { get; set; }
+
+        //Constructor:
+        public UserViewModel()
+        {
+            SaveTableCommand = new RelayCommand(SaveTabe);
+        }
+
+        private void SaveTabe(object obj)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                MessageBox.Show(IsCheckedArray[i].ToString());
             }
         }
     }
