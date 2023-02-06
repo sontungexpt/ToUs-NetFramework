45f2f8f3a36153e4bfea25e1d6cc727ae45760ae
HiImMalip <winqti@gmail.com>
2023-02-06 10:43:04 +0700

    StartView_ConfirmBox

--- ToUs/Resources/CustomControl/ConfirmBox.xaml.cs
+++ ToUs/Resources/CustomControl/ConfirmBox.xaml.cs
@@ -20,9 +20,85 @@ namespace ToUs.Resources.CustomControl
     /// </summary>
     public partial class ConfirmBox : UserControl
     {
+
+        //Code
+
+        public new string Code
+        {
+            get { return (string)GetValue(CodeProperty); }
+            set { SetValue(CodeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Code. This enables animation,
+        // styling, binding, etc...
+        public static readonly DependencyProperty CodeProperty =
+            DependencyProperty.Register("Code", typeof(string), typeof(ConfirmBox), new PropertyMetadata(string.Empty));
+
         public ConfirmBox()
         {
             InitializeComponent();
         }
+
+        private void TBCode1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TBCode1.Text) && TBCode1.Text.Length > 0)
+            {
+                TBCode2.Focus();
+                Code += TBCode1.Text;
+            }
+        }
+
+        private void TBCode2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(TBCode2.Text) && TBCode2.Text.Length > 0)
+            {
+                TBCode3.Focus();
+                Code += TBCode2.Text;
+            }
+        }
+
+        private void TBCode3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+                if (!string.IsNullOrEmpty(TBCode3.Text) && TBCode3.Text.Length > 0)
+                {
+                    TBCode4.Focus();
+                    Code += TBCode3.Text;
+
+
+                }
+        }
+
+        private void TBCode4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TBCode4.Text) && TBCode4.Text.Length > 0)
+            {
+                TBCode5.Focus();
+                Code += TBCode4.Text;
+
+            }
+        }
+
+        private void TBCode5_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TBCode5.Text) && TBCode5.Text.Length > 0)
+            {
+                TBCode6.Focus();
+                Code += TBCode5.Text;
+
+            }
+
+        }
+
+        private void TBCode6_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TBCode5.Text) && TBCode5.Text.Length > 0)
+            {
+                Code += TBCode6.Text;
+            }
+
+        }
+
+
     }
 }
